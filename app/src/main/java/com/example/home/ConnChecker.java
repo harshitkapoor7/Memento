0cb0d84fd37588e8b3d8a58df6569b1054df0777
Harshit <meharshitkapoor7@gmail.com>
2020-07-04 18:57:07 +0530

    Dup

---
+++ app/src/main/java/com/example/home/ConnChecker.java
@@ -0,0 +1,21 @@
+package com.example.home;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnChecker {
+    public static boolean check(Context context){
+        ConnectivityManager manager=(ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(manager!=null){
+            NetworkInfo[] info=manager.getAllNetworkInfo();
+            if(info!=null){
+                for(int i=0;i<info.length;i++){
+                    if(info[i].getState() == NetworkInfo.State.CONNECTED)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/home/ConnectionChecker.java
@@ -0,0 +1,43 @@
+package com.example.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ConnectionChecker extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection_checker);
+        btn=findViewById(R.id.btn);
+        getSupportActionBar().hide();
+//        getActionBar().hide();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ConnChecker.check(getBaseContext()) == true) {
+
+                    Intent intent=new Intent(view.getContext(),MainActivity.class);
+                    getSupportActionBar().show();
+//                    getActionBar().show();
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"No Internet Connection",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
