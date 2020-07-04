19c328d0398505f65c13a881a1f29c51a211e2b9
Harshit <meharshitkapoor7@gmail.com>
2020-07-04 18:54:14 +0530

    Dup

--- app/src/main/java/com/example/home/MainActivity.java
+++ app/src/main/java/com/example/home/MainActivity.java
@@ -278,6 +278,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             drawerLayout.closeDrawer(GravityCompat.START);
         }
         return true;
+//        smak
     }
 
     @Override
